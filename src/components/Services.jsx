315e94fab0aca3f1106d891f571c720223cf0bae
muhammadhussainjs <hussain356q@gmail.com>
2024-04-15 03:58:44 +0500

    processing

--- src/components/Services.jsx
+++ src/components/Services.jsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 import weather from "../assets/weather-app.png"
 import blogging from "../assets/blogingapp.png"
 import phone from "../assets/phoneapp.png"
+import react from "../assets/react.png"
 
 
 const Services = () => {
@@ -25,15 +26,7 @@ const Services = () => {
         </h1>
         <div className=" md:space-x-5 md:px-10 gap-12  items-center px-5 
         flex flex-wrap sm:flex-row  mt-12 justify-around" >
-          <div className=" cursor-pointer hover:bg-gray-100 bg-slate-100 p-5 text-center shadow-lg
-           rounded-xl  service1 space-y-4"> 
-        
-          
-            <img src={weather} alt="" className="rounded-xl w-60" />
-            <button className="px-3 py-2 bg-orange-500 text-2xl rounded-full shadow-lg">
-            <a href="https://muhammadhussainjs.github.io/WEATHER-APP/">visit website</a>
-            </button>
-          </div>
+         
           <div className=" cursor-pointer hover:bg-gray-100 bg-slate-100 p-5 text-center shadow-lg
            rounded-xl   service1 space-y-4"> 
         
@@ -41,6 +34,24 @@ const Services = () => {
             <img src={blogging} alt="" className="rounded-xl w-60"/>
             <button className="px-3 py-2 bg-orange-500 text-2xl rounded-full shadow-lg">
             <a href=" https://muhammadhussainjs.github.io/Blogging-App/">visit website</a>
+            </button>
+          </div>
+          <div className=" cursor-pointer hover:bg-gray-100 bg-slate-100 p-5 text-center shadow-lg
+           rounded-xl   service1 space-y-4"> 
+        
+          
+            <img src={react} alt="" className="rounded-xl w-60"/>
+            <button className="px-3 py-2 bg-orange-500 text-2xl rounded-full shadow-lg">
+            <a href="https://ecommerceonreact2.web.app/">visit website</a>
+            </button>
+          </div>
+          <div className=" cursor-pointer hover:bg-gray-100 bg-slate-100 p-5 text-center shadow-lg
+           rounded-xl  service1 space-y-4"> 
+        
+          
+            <img src={weather} alt="" className="rounded-xl w-60" />
+            <button className="px-3 py-2 bg-orange-500 text-2xl rounded-full shadow-lg">
+            <a href="https://muhammadhussainjs.github.io/WEATHER-APP/">visit website</a>
             </button>
           </div>
           <div className=" cursor-pointer hover:bg-gray-100 bg-slate-100 p-5 text-center shadow-lg
