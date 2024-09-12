6bf37d4f64a1adda01f460d2006d29dc051ba54e
muhammadhussainjs <hussain356q@gmail.com>
2024-09-12 14:34:30 +0300

    test

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -2,7 +2,7 @@ import bannerImage from "../assets/profile-pic.png";
 import bannerBackground from "../assets/banner_wallpaper.svg";
 import Typed from "typed.js"
 import { useRef, useEffect , useState } from "react";
-import  Resume from "../assets/MuhammadHussainResume.pdf"
+import  Resume from '../assets/Muhammad.pdf'
 
 const Banner = () => {
   const el = useRef(null);
